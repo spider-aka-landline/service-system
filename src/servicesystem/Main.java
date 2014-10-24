0e1a4159d6c3b5e558b75c808f5afa0fa1fe4dae
Spider <ghost.of.spider@gmail.com>
2014-10-24 17:43:03 +0300

    Fixing merging bugs

--- src/servicesystem/Main.java
+++ src/servicesystem/Main.java
@@ -10,7 +10,6 @@ public class Main {
         int u1 = 30000;
         int u2 = 300000;
 
-<<<<<<< HEAD
         Boolean isVariance = true;
 
         //Dipole (users,providers). min,max.
@@ -22,20 +21,6 @@ public class Main {
         SystemsBruteForcer maks2 = new SystemsBruteForcer(true, isVariance,
                 new DipoleData(u2, p), new DipoleData(u2, p), 1000, 1000);
         maks2.run();
-
-=======
-    //Dipole (users,providers). min,max.
-        SystemsBruteForcer maks1 = new SystemsBruteForcer(true,
-                new DipoleData(u1, p), new DipoleData(u1, p), 1000, 1000);
-        maks1.run();
-        
-        //Dipole (users,providers). min,max.
-        SystemsBruteForcer maks2 = new SystemsBruteForcer(true,
-                new DipoleData(u2, p), new DipoleData(u2, p), 1000, 1000);
-        maks2.run();
-        
-        
->>>>>>> bd7ead029752b4fcb4cdf8a5d51791baa7e0947d
     }
 
 }
